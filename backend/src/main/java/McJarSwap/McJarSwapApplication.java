df0593a4c6559996c102b9e275ff8a3224bed061
dasol <pds2023@gmail.com>
2025-01-22 17:02:12 +0900

    bulid: spring 환경구축

---
+++ backend/src/main/java/McJarSwap/McJarSwapApplication.java
@@ -0,0 +1,13 @@
+package McJarSwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McJarSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McJarSwapApplication.class, args);
+	}
+
+}
