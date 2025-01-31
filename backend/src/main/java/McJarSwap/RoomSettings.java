6a61592e9cf9706b47a704b74520fc1aa3b417d3
Sangmin Kang <kkang5430@naver.com>
2025-01-31 16:55:16 +0900

    변수명 changePort -> changeport

--- backend/src/main/java/McJarSwap/RoomSettings.java
+++ backend/src/main/java/McJarSwap/RoomSettings.java
@@ -3,21 +3,21 @@ package McJarSwap;
 public class RoomSettings {
     //room setting 을 위해 받는 data
     private String port;
-    private String changePort;
+    private String changeport;
     private String mode;
 
 
-    public RoomSettings(String port, String changePort, String mode) {
+    public RoomSettings(String port, String changeport, String mode) {
         this.port = port;
-        this.changePort = changePort;
+        this.changeport = changeport;
         this.mode = mode;
     }
 
     public String getPort() { return port; }
     public void setPort(String port) { this.port = port; }
 
-    public String getChangePort() { return changePort; }
-    public void setChangePort(String changePort) { this.changePort = changePort; }
+    public String getChangeport() { return changeport; }
+    public void setChangeport(String changeport) { this.changeport = changeport; }
 
     public String getMode() { return mode; }
     public void setMode(String mode) { this.mode = mode; }
